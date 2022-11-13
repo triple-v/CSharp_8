10af8b3f8b2f449f3f62f5065c5f7701b26eacc6
triple-v <vasilevich.account@ya.ru>
2022-11-13 23:42:30 +0300

    Работа над ошибками в задаче 56

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.WriteLine();
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.WriteLine();
 int[,] CreateMtrixRndInt(int row, int col, int min, int max)
 {
   int[,] matrix = new int[row, col];
@@ -31,7 +39,7 @@ int SearchMinimumSumString(int[,] matrix)
   for (int i = 0; i < matrix.GetLength(0); i++)
   {
     int sum = 0;
-    for (int j = 0; j < matrix.GetLength(0); j++)
+    for (int j = 0; j < matrix.GetLength(1); j++)
     {
       sum = sum + matrix[i, j];
     }
